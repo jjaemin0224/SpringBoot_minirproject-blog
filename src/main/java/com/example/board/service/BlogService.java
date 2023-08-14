12b323fad5c4b3633153bc98e49b23a3c25a7805
jjaemin0224 <jjaemin0224@nate.com>
2023-08-14 17:32:14 +0900

    BlogApi

---
+++ src/main/java/com/example/board/service/BlogService.java
@@ -0,0 +1,46 @@
+package com.example.board.service;
+
+import com.example.board.domain.Article;
+import com.example.board.dto.AddArticleRequest;
+import com.example.board.dto.UpdateArticleRequest;
+import com.example.board.repository.BlogRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor  // final이 붙거나 @NotNull이 붙은 필드의 생성자 추가
+@Service  // 빈으로 등록
+public class BlogService {
+
+    private final BlogRepository blogRepository;
+
+    // 블로그 글 추가 메서드
+    public Article save(AddArticleRequest request) {
+        return blogRepository.save(request.toEntity());
+    }
+
+    public List<Article> findAll() {
+        return blogRepository.findAll();
+    }
+
+    public Article findById(long id) {
+        return blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+    }
+
+    public void delete(long id) {
+        blogRepository.deleteById(id);
+    }
+
+    @Transactional  // 트랜잭션 메서드
+    public Article update(long id, UpdateArticleRequest request) {
+        Article article = blogRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("not found: " + id));
+
+        article.update(request.getTitle(), request.getContent());
+
+        return article;
+    }
+}
