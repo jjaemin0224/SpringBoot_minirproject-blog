067c7d4e63ac6a74fb0faac08951d37307045052
jjaemin0224 <jjaemin0224@nate.com>
2023-08-17 20:55:41 +0900

    JWT (login, logout)

---
+++ src/main/java/com/example/board/dto/CreateAccessTokenRequest.java
@@ -0,0 +1,10 @@
+package com.example.board.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateAccessTokenRequest {
+    private String refreshToken;
+}
---
+++ src/main/java/com/example/board/service/RefreshTokenService.java
@@ -0,0 +1,19 @@
+package com.example.board.service;
+
+import com.example.board.domain.RefreshToken;
+import com.example.board.repository.RefreshTokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class RefreshTokenService {
+
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public RefreshToken findByRefreshToken(String refreshToken) {
+        return refreshTokenRepository.findByRefreshToken(refreshToken)
+                .orElseThrow(() -> new IllegalArgumentException("Unexpected token"));
+    }
+
+}
