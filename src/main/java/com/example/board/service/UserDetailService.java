d0aff78b5d6a98701a4d737fb015a4850bf95989
jjaemin0224 <jjaemin0224@nate.com>
2023-08-17 19:57:47 +0900

    UserApi (spring security)

---
+++ src/main/java/com/example/board/service/UserDetailService.java
@@ -0,0 +1,20 @@
+package com.example.board.service;
+
+import com.example.board.repository.UserRepository;
+import com.example.board.domain.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+}
